a1ccf069fd90a20f69bc3dd1e63ccbc92e1fd87b
Yakir <got.yakir@gmail.com>
2023-11-01 15:34:39 +0200

    test: remove unneeded test

--- apps/todo-ui/src/app/app.component.spec.ts
+++ apps/todo-ui/src/app/app.component.spec.ts
@@ -8,13 +8,4 @@ describe("AppComponent", () => {
 			imports: [AppComponent, RouterTestingModule],
 		}).compileComponents();
 	});
-
-	it("should render title", () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		fixture.detectChanges();
-		const compiled = fixture.nativeElement as HTMLElement;
-		expect(compiled.querySelector("h1")?.textContent).toContain(
-			"Welcome todo-ui",
-		);
-	});
 });
