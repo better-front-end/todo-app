c5cb8553c23821d2d7024a23ee9fee0e01f00f09
Yakir <got.yakir@gmail.com>
2023-11-01 13:19:20 +0200

    style: improve code style

--- apps/todo-api/src/main.ts
+++ apps/todo-api/src/main.ts
@@ -5,10 +5,16 @@
 
 import express from "express";
 import * as path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const app = express();
 
-app.use("/assets", express.static(path.join(__dirname, "assets")));
+app.use(
+	"/assets",
+	express.static(
+		path.join(path.dirname(fileURLToPath(import.meta.url)), "assets"),
+	),
+);
 
 app.get("/api", (request, response) => {
 	response.send({ message: "Welcome to todo-api!" });
@@ -16,6 +22,8 @@ app.get("/api", (request, response) => {
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
+	// eslint-disable-next-line no-console
 	console.log(`Listening at http://localhost:${port}/api`);
 });
+// eslint-disable-next-line no-console
 server.on("error", console.error);
